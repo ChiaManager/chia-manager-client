7bda582c17197e9280c21f2c72311db1c0cdc753
Luca Austelat <luca@austelat.de>
2021-10-31 12:37:41 +0100

    remove not longer existing constants from import

--- node/__init__.py
+++ node/__init__.py
@@ -1,5 +1,5 @@
 __version__ ="0.1.2"
 __all__ = [
-    "ApiHandler", "ChiaHandler", "NodeConfig", "NodeConstants", "NodeHelper", "NodeLogger", "NodeWebsocket",
+    "ApiHandler", "ChiaHandler", "NodeConfig", "NodeHelper", "NodeLogger", "NodeWebsocket",
     "Singleton", "SystemInfo", "version"
 ]
