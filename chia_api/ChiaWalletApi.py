5aa7666dfdccb42f3ccd215b2bcb049b4ad225ba
Luca Austelat <luca@austelat.de>
2021-11-27 19:18:14 +0100

    remove unnecessary function from wallet api class

--- chia_api/ChiaWalletApi.py
+++ chia_api/ChiaWalletApi.py
@@ -15,28 +15,28 @@ class ChiaWalletApi(ChiaApi):
         # todo: read ports from ~/.chia/mainnet/config/config.yaml
         self.port = 9256
         
-    def __send_wallet_request(self, url_path: str, data: Union[dict] = None):
-        return self._send_request(self.port, url_path=url_path, data=data)
-        
     def get_wallets(self):
-        return self.__send_wallet_request('get_wallets')
+        return self._send_request('get_wallets')
+
+    def get_farmed_amount(self, wallet_id: int):
+        return self._send_request('get_farmed_amount', data={'wallet_id': wallet_id})
 
     def get_transactions(self, wallet_id: int):
-        return self.__send_wallet_request('get_transactions', data={'wallet_id': wallet_id})
+        return self._send_request('get_transactions', data={'wallet_id': wallet_id})
 
     def get_height_info(self, wallet_id: int):
         # get sync height
-        return self.__send_wallet_request('get_height_info', data={'wallet_id': wallet_id}).get('height')
+        return self._send_request('get_height_info', data={'wallet_id': wallet_id}).get('height')
 
     def get_wallet_balance(self, wallet_id: int):
-        return self.__send_wallet_request('get_wallet_balance', data={'wallet_id': wallet_id}).get('wallet_balance')
+        return self._send_request('get_wallet_balance', data={'wallet_id': wallet_id}).get('wallet_balance')
 
     def get_next_address (self, wallet_id: int):
         # get current wallet adress
-        return self.__send_wallet_request(
+        return self._send_request(
             'get_next_address', 
             data={'wallet_id': wallet_id, 'new_address': False}
         ).get('address')
 
     def get_sync_status (self, wallet_id: int):
-        return self.__send_wallet_request('get_sync_status', data={'wallet_id': wallet_id})
+        return self._send_request('get_sync_status', data={'wallet_id': wallet_id})
