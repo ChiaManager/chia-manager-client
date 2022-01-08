f591d06dfe382b697823ed0bb817a5241390500a
Luca Austelat <luca@austelat.de>
2022-01-08 12:13:07 +0100

    add harvester start with chia daemon

--- chia_api/ChiaHarvesterApi.py
+++ chia_api/ChiaHarvesterApi.py
@@ -1,11 +1,16 @@
 
 from chia_api.ChiaApi import ChiaApi, log
+from chia_api.ChiaDaemon import ChiaDaemon
+from chia_api.constants import ServicesForGroup
 
 
 class ChiaHarvesterApi(ChiaApi):
     def __init__(self):
         super(ChiaHarvesterApi, self).__init__()
         self.port = 8560
+
+    def start(self, restart: bool = False):
+        return ChiaDaemon().start_service(service=ServicesForGroup.HARVESTER, restart=restart)
 
     def get_plots(self):
         return self._send_request('get_plots').get('plots', {})
