19fd61210804ba2aaf1cf286c6b6de58d978e6ef
Luca Austelat <luca@austelat.de>
2022-03-07 10:43:44 +0100

    System information adapted to Windows

--- system/SystemInfo.py
+++ system/SystemInfo.py
@@ -1,11 +1,19 @@
-import logging
+import sys
 import os
-import subprocess
 import time
+import logging
+import subprocess
+import platform
+
+from system.constants import WIN_RELEASES
 
 import psutil
 
+
 log = logging.getLogger()
+
+IS_WINDOWS = sys.platform == 'win32'
+
 
 
 class SystemInfo:
@@ -13,12 +21,12 @@ class SystemInfo:
     def get_system_info(self) -> dict:
         sys_info = {
             'system': {
-                'load': self.get_load_avg(),
                 'memory': self.get_memory_info(),
                 'swap': self.get_swap_info(),
                 'filesystem': self.get_filesystem_info(),
                 'cpu': self.get_cpu_info(),
-                'uptime': time.time() - psutil.boot_time()
+                'uptime': time.time() - psutil.boot_time(),
+                'os': self.get_os(),
             }
         }
 
@@ -26,36 +34,55 @@ class SystemInfo:
         return sys_info
 
     @staticmethod
-    def get_load_avg() -> dict:
-        load_avg = psutil.getloadavg()
-        return {
-            '1min': load_avg[0],
-            '5min': load_avg[1],
-            '15min': load_avg[2]
-        }
+    def get_os():
+        res = {'type': platform.system()}
+        if IS_WINDOWS:
+                
+            win_ver = sys.getwindowsversion()
+            maj, min, build = win_ver.platform_version or win_ver[:3]
 
+            res['name'] = WIN_RELEASES.get((maj, min, build)) or win_ver
+        else:
+            res['name'] = platform.platform()
+
+        return res
     @staticmethod
     def get_filesystem_info() -> list:
-        filesystems = subprocess.getoutput("df | tail -n +2").splitlines()
-
         file_system_info = []
-        for filesystem in filesystems:
-            # add all not empty entries
-            file_system_info.append([e for e in filesystem.split(" ") if e != ""])
+        
+        for disk in psutil.disk_partitions():
+            disk_usage = psutil.disk_usage(disk.device)
+
+            file_system_info.append({
+                'device': disk.device,
+                'total': disk_usage.total,
+                'used': disk_usage.used,
+                'free':  disk_usage.free,
+                'mountpoint': disk.mountpoint,
+                'fs_type': disk.fstype
+            })
 
         return file_system_info
 
     @staticmethod
     def get_memory_info() -> dict:
         mem_info = psutil.virtual_memory()
-
-        return {
+        result = {
             'total': mem_info.total,
             'free': mem_info.free,
-            'buffers': mem_info.buffers,
-            'cached': mem_info.cached,
-            'shared': mem_info.shared
+            'buffers': None,
+            'cached': None,
+            'shared': None,
         }
+
+        if not IS_WINDOWS:
+            result.update({
+                'buffers': mem_info.buffers,
+                'cached': mem_info.cached,
+                'shared': mem_info.shared
+            })
+        
+        return result
 
     @staticmethod
     def get_swap_info() -> dict:
@@ -67,13 +94,17 @@ class SystemInfo:
 
     @staticmethod
     def get_cpu_info() -> dict:
-        # count and cores are the same ?
+        load_avg = psutil.getloadavg()
+
         return {
-            "count": os.cpu_count(),
+            'count': os.cpu_count(),
             'physical_cores': psutil.cpu_count(),
             'logical_cores': psutil.cpu_count(logical=True),
-            "cores": subprocess.getoutput(
-                "cat /proc/cpuinfo | egrep 'cpu cores' | cut -d':' -f 2 | sort | uniq"),
-            "model": subprocess.getoutput(
-                "cat /proc/cpuinfo | egrep 'model name' | cut -d':' -f 2 | sort | uniq")
+            'model': platform.processor(),
+            'usage': psutil.cpu_percent(),
+            'load': {
+                '1min': load_avg[0],
+                '5min': load_avg[1],
+                '15min': load_avg[2]
+            }
         }
